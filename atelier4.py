3525eeab555d712bebebb2454cf0c55894946fe2
malika2714 <malikahaddadi0109@gmail.com>
2026-03-08 01:58:15 -0500

    Création de la méthode afficherinfo

--- atelier4.py
+++ atelier4.py
@@ -4,3 +4,13 @@ class Employee:
         self.nom=nom
         self.prenom=prenom
         self.voitureservice=None
+
+    def afficherInformations(self):
+        print("Numero de permis :", self.numpermis)
+        print("Nom :", self.nom)
+        print("Prenom :", self.prenom)
+
+        if self.voitureservice is None:
+            print("Voiture de service : aucune")
+        else:
+            print("Voiture de service :", self.voitureservice.matricule)
