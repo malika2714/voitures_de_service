aacea13e69abdc8b6d0ba06c77cf776ce12576d6
malika2714 <malikahaddadi0109@gmail.com>
2026-03-08 01:54:02 -0500

    Création de la classe Employe avec ses attributs

---
+++ atelier4.py
@@ -0,0 +1,6 @@
+class Employee:
+    def __init__(self, numpermis, nom, prenom):
+        self.numpermis=numpermis
+        self.nom=nom
+        self.prenom=prenom
+        self.voitureservice=None
